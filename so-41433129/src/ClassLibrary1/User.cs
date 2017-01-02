567af44aef119870703a1a197c5c1d80531338d4
Dave van Herten <dvanherten@outlook.com>
2017-01-02 17:13:35 -0500

    Answer SO-41433129

---
+++ so-41433129/src/ClassLibrary1/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace ClassLibrary1
+{
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ so-41433129/src/ClassLibrary1/UserIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassLibrary1
+{
+    public class UserIdentityDbContext : IdentityDbContext<User>
+    {
+        public UserIdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
